e5792fee36f37f2c1af11fad709e3fe54ef86890
Maksat <maksiqkgz@gmail.com>
2022-05-29 19:33:30 +0600

    delete model category and fix it all problems with him

--- shop/admin.py
+++ shop/admin.py
@@ -3,14 +3,13 @@ from django.contrib import admin
 from .models import *
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'img', 'is_available', 'cat')
+    list_display = ('id', 'title', 'time_create', 'img', 'is_available')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_available',)
+    list_filter = ('is_available', 'time_create')
+    prepopulated_fields = {"slug": ("title",)}
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    list_display_links = ('id', 'name')
-    search_fields = ('name',)
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
+
--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,13 @@
-from xml.etree.ElementTree import Comment
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 from .models import *
 
 def shop(request):
-    products = Product.objects.all().order_by('id')
+    products = Product.objects.all().order_by('slug')
     paginator = Paginator(products, 6)
+    # paginator = Paginator(products, 6)
     page = request.GET.get('page')
     try:
         products = paginator.page(page)
@@ -15,26 +15,25 @@ def shop(request):
         products = paginator.page(1)
     except EmptyPage: 
         products = paginator.page(paginator.num_pages)
-    return render(request, 'shop/shop.html', {'products': products})
-
-# class ShopHome(ListView):
-#     model = Product
-#     template_name = 'shop/shop.html'
+    return render(request, 'shop/shop.html', {'products': products,})
 
 
 
-def product(request, product_id):
 
-    product = Product.objects.get(id=product_id)
+def product(request, product_slug):
+    product = get_object_or_404(Product, slug=product_slug)
+    
+    # product = Product.objects.get(id=product_id)
     # product.update(view_count=product.view_count+1)
-    # com = product.comments.filter(product_id=id)
+    # com = product.comments.filter(product_slug=slug)
 
-    if request.method == "POST":
-        comment = Review()
-        comment.product_id = product_id
-        comment.author = request.POST.get('author')
-        comment.email = request.POST.get('email')
-        comment.comment = request.POST.get('comment')
-        comment.save()
-    return render(request, 'shop/shop-details.html', {'product': product})
+    # if request.method == "POST":
+    #     comment = Review()
+    #     comment.product_id = id
+    #     comment.author = request.POST.get('author')
+    #     comment.email = request.POST.get('email')
+    #     comment.comment = request.POST.get('comment')
+    #     comment.save()
+    return render(request, 'shop/shop-details.html', {'product':product})
+
 
