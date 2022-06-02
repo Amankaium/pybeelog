4d1a2d51073aaa6ed3cd4192a052144dc20701be
Maksat <maksiqkgz@gmail.com>
2022-06-02 22:51:39 +0600

    imported shop models and connected index.html

--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from shop.models import *
+#from blog.models import *
 
+
+
+
+def homepage(request):
+    product = Product.objects.all().order_by('id')
+    trending_products = Product.objects.all().order_by('view_count')[:8]
+    featured_products = Product.objects.all().order_by('view_count')[8:]
+    best_seller_products = Product.objects.all().order_by('view_count')[:6]
+    #latest_blog = Post.objects.all().order_by('visit_count')[:4]
+    return render(request, 'core/index.html', {'product': product, 'trending_products': trending_products, \
+                                                'featured_products':featured_products, 'best_seller_products': best_seller_products})
 
 # Create your views here.
