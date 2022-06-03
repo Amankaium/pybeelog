c42c417dde5bdccde37048317e826af0bd127c2d
anara01 <anaraerkebaeva2@gmail.com>
2022-06-03 13:48:30 +0300

    contact-faq-404

--- pybeelog/urls.py
+++ pybeelog/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from contact.views import *
+
 urlpatterns = [
+    path('', index, name='home'),
     path('admin/', admin.site.urls),
+    path('contact/', include('contact.urls')),
+    path('faq/', include('faq.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = handle_not_found
