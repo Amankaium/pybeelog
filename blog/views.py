9280e64b7f728759f50a3e13ba7a32517daf5187
Natalia <nata.mamseva.93@bk.ru>
2022-06-11 13:43:39 +0600

    changed the function of Post and Comment in views.py

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.decorators.csrf import csrf_exempt
 
 
 def Blog(request):
@@ -12,28 +13,29 @@ def Blog(request):
         posts = paginator.page(page)
     except PageNotAnInteger:
         posts = paginator.page(1)
-#    except EmptyPage: 
-#        blog = paginator.page(paginator.num_pages)
+    except EmptyPage: 
+        blog = paginator.page(paginator.num_pages)
     return render(request, 'blog/blog-1.html', {'posts': 'posts'})
 
 
 
-
-def Post(request, id):
-    
+@csrf_exempt
+def PostView(request, id):
     details = Post.objects.get(id=id)
-    Post.view_count = Post.view_count + 1
-    Post.save()
-    review = Post.review.filter(post_id=id)
-    
+    details.visits +=1
+    details.save()
+    comments = details.comments
+    popular_posts=Post.objects.order_by('-visits').all()[5:]
+
     
     if request.method == "POST":
-        comment = review()
-        comment.post_id = id
-        comment.author = request.POST.get('name')
-        comment.email = request.POST.get('email')
-        comment.website = request.POST.get('website')
-        comment.comment = request.POST.get('comment')
+        comment = Comment(
+        author=request.POST.get('author'),
+        email=request.POST.get('email'),
+        comment=request.POST.get('comment')
+        )
+       
+        comment.post = details
         comment.save()
-    return render(request, 'blog/blog-details.html', {'details':details})
+    return render(request, 'blog/blog-details.html', {'details':details, 'popular_posts': popular_posts})
  
