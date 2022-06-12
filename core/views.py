6cf6e813b0a2e08870d061caeb3258b090a87cdb
Aimedin Chorobaev <chaimedin2003@gmail.com>
2022-06-12 12:26:52 +0600

    fixed core

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from shop.models import *
-#from blog.models import *
+from shop.models import Product
+from blog.models import Post
 
 
 
@@ -8,10 +8,10 @@ from shop.models import *
 def homepage(request):
     product = Product.objects.all().order_by('id')
     trending_products = Product.objects.all().order_by('view_count')[:8]
-    featured_products = Product.objects.all().order_by('view_count')[8:]
+    featured_products = Product.objects.all().order_by('view_count')[:8]
     best_seller_products = Product.objects.all().order_by('view_count')[:6]
-    #latest_blog = Post.objects.all().order_by('visit_count')[:4]
-    return render(request, 'core/index.html', {'product': product, 'trending_products': trending_products, \
-                                                'featured_products':featured_products, 'best_seller_products': best_seller_products})
+    latest_blog = Post.objects.all().order_by('visit_count')[:3]
 
-# Create your views here.
+    
+    return render(request, 'core/index.html', {'product': product, 'trending_products': trending_products, \
+                                                'featured_products':featured_products, 'best_seller_products': best_seller_products, 'latest_blog': latest_blog})
